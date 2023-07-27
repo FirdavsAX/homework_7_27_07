a51ae7999324d0c74e1a78ce47804ad5ab92f2a0
FirdavsAX <firdavsjumayev320@gmail.com>
2023-07-27 17:00:34 +0500

    have a buds

--- homework_7_27_07/Program.cs
+++ homework_7_27_07/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] strings = new string[] {"fluutter", "fluuy","fluuinheard"};
+            string helpStr = strings[0].Substring(0,1);
+
+            int k = 1;
+
+            bool b = false;
+            ///flinhard glomgold
+            
+            while(true)
+            {
+                for (int i = 0; i<strings.Length; i++)
+                {
+                    if (strings[i].Substring(0,k) == helpStr)
+                    {
+                        b = true;
+                    }
+                    else
+                    {
+                        b=false;
+                        break;
+                    }
+                }
+                if (b)
+                {
+                    helpStr += strings[0].Substring(0, k);
+                    k++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(k);
         }
+        
     }
 }
